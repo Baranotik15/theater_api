79a7cd61cbbd5d954bb859f636d5a1fc4bbe0452
RuslanTopal <baranotik15@gmail.com>
2025-05-09 18:54:20 +0000

    added unique filds

--- theater/models.py
+++ theater/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class TheatreHall(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     rows = models.PositiveIntegerField()
     seats_in_row = models.PositiveIntegerField()
 
@@ -14,19 +14,22 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
+    class Meta:
+        unique_together = ('first_name', 'last_name')
+
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
 
 
 class Play(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, unique=True)
     description = models.TextField()
 
     def __str__(self):
@@ -37,6 +40,9 @@ class Performance(models.Model):
     play = models.ForeignKey(Play, on_delete=models.CASCADE)
     theatre_hall = models.ForeignKey(TheatreHall, on_delete=models.CASCADE)
     show_time = models.DateTimeField()
+
+    class Meta:
+        unique_together = ('play', 'theatre_hall', 'show_time')
 
     def __str__(self):
         return (
