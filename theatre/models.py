980ba66105381d26e33301d317e2f767d8fa3a4d
RuslanTopal <baranotik15@gmail.com>
2025-05-09 16:17:49 +0000

    added models for theatre app

--- theatre/models.py
+++ theatre/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class TheatreHall(models.Model):
+    name = models.CharField(max_length=100)
+    rows = models.PositiveIntegerField()
+    seats_in_row = models.PositiveIntegerField()
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Play(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Performance(models.Model):
+    play = models.ForeignKey(Play, on_delete=models.CASCADE)
+    theatre_hall = models.ForeignKey(TheatreHall, on_delete=models.CASCADE)
+    show_time = models.DateTimeField()
