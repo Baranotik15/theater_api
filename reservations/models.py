6895b74aac131b216dc28166729256e897dd99b0
RuslanTopal <baranotik15@gmail.com>
2025-05-09 16:29:03 +0000

    created all models of project

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from theatre.models import Performance
 
-# Create your models here.
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Reservation by {self.user.username} on {self.created_at}"
+
+
+class Ticket(models.Model):
+    row = models.PositiveIntegerField()
+    seat = models.PositiveIntegerField()
+    performance = models.ForeignKey(Performance, on_delete=models.CASCADE)
+    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Ticket for {self.performance.title} at Row {self.row}, Seat {self.seat}"
